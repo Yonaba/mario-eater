46175008c18038132d6abeb337c1475e645c4e48
Yonaba <roland.yonaba@gmail.com>
2014-07-24 11:30:27 +0000

    added over

---
+++ src/over.lua
@@ -0,0 +1,12 @@
+while true do
+	screen:clear()
+	pad = Controls.read()
+
+	if pad:cross() then dofile("script.lua") end
+
+	screen:print(220,100,"Game Over", Color.new(255,255,255))
+	screen:print(210,110,"Press Cross", Color.new(255,255,255))
+
+	screen.waitVblankStart()
+	screen.flip()
+end
