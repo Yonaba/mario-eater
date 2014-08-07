417485f95f5042eb31af3c50b58c61916d324bd4
Yonaba <roland.yonaba@gmail.com>
2014-08-07 12:25:55 +0000

    Tabs to 2spaces

--- src/lvl2.lua
+++ src/lvl2.lua
@@ -8,106 +8,106 @@ mario = Image.load("img/mario.png")
 bomb = Image.load("img/bomb.png")
 
 player = {
-	x = 236;
-	y = 220;
-	score = 0;
-	img = player;
-	height = 67;
-	width = 54;
-	health = 100;
+  x = 236;
+  y = 220;
+  score = 0;
+  img = player;
+  height = 67;
+  width = 54;
+  health = 100;
 }
 
 mario = {
-	x = math.random (0,480);
-	y = -20;
-	height = 50;
-	width = 25;
-	img = mario;
+  x = math.random (0,480);
+  y = -20;
+  height = 50;
+  width = 25;
+  img = mario;
 }
 
 mario2 = {
-	x = math.random (0,480);
-	y = -20;
-	height = 50;
-	width = 25;
-	img = mario;
+  x = math.random (0,480);
+  y = -20;
+  height = 50;
+  width = 25;
+  img = mario;
 }
 
 bomb = {
-	x = math.random (0,480);
-	y = -20;
-	height = 57;
-	width = 56;
-	img = bomb;
+  x = math.random (0,480);
+  y = -20;
+  height = 57;
+  width = 56;
+  img = bomb;
 }
 
 function collideEngine(flame)
-	if (player.x + player.width >flame.x) and 
-	   (player.x < flame.x + flame.width) and 
-		 (player.y + player.height > flame.y) and
-		 (player.y < flame.y + flame.height) then 	
-			player.score = player.score +10
-			mario.x = math.random(0,480)
-			mario.y = -50	
-			mario2.x = math.random(0,480)
-			mario2.y = -50		
-	end
+  if (player.x + player.width >flame.x) and 
+     (player.x < flame.x + flame.width) and 
+     (player.y + player.height > flame.y) and
+     (player.y < flame.y + flame.height) then   
+      player.score = player.score +10
+      mario.x = math.random(0,480)
+      mario.y = -50  
+      mario2.x = math.random(0,480)
+      mario2.y = -50    
+  end
 end
 
 function collideBomb(flame)
-	if (player.x + player.width >flame.x) and 
-	   (player.x < flame.x + flame.width) and 
-	   (player.y + player.height > flame.y) and 
-	   (player.y < flame.y + flame.height) then 	
-				player.health = player.health - 5
-				bomb.x = math.random(0,480)
-				bomb.y = -30	
-	end
+  if (player.x + player.width >flame.x) and 
+     (player.x < flame.x + flame.width) and 
+     (player.y + player.height > flame.y) and 
+     (player.y < flame.y + flame.height) then   
+        player.health = player.health - 5
+        bomb.x = math.random(0,480)
+        bomb.y = -30  
+  end
 end
 
 
 
 while true do
-	screen:clear()
+  screen:clear()
 
-	pad = Controls.read()
-	if pad:left() then player.x = player.x - 2 end
-	if pad:right() then player.x = player.x + 2 end
-	if player.x <= 0 or player.x >=480-54 then player.x = oldx end
-	oldx = player.x
+  pad = Controls.read()
+  if pad:left() then player.x = player.x - 2 end
+  if pad:right() then player.x = player.x + 2 end
+  if player.x <= 0 or player.x >=480-54 then player.x = oldx end
+  oldx = player.x
 
-	mario.y = mario.y + 2
-	mario2.y = mario2.y + 2
-	bomb.y = bomb.y + 3
+  mario.y = mario.y + 2
+  mario2.y = mario2.y + 2
+  bomb.y = bomb.y + 3
 
-	if mario.y >= 290 then 
-		mario.x = math.random(0,480) 
-		mario.y = -50
-	end
-	if mario2.y >= 290 then 
-		mario2.x = math.random(0,480)
-		mario2.y = -50 
-	end
-	if bomb.y >= 285 then 
-		bomb.x = math.random(0,480)
-		bomb.y = - 30 
-	end
+  if mario.y >= 290 then 
+    mario.x = math.random(0,480) 
+    mario.y = -50
+  end
+  if mario2.y >= 290 then 
+    mario2.x = math.random(0,480)
+    mario2.y = -50 
+  end
+  if bomb.y >= 285 then 
+    bomb.x = math.random(0,480)
+    bomb.y = - 30 
+  end
 
-	collideEngine(mario)
-	collideEngine(mario2)
-	collideBomb(bomb)
+  collideEngine(mario)
+  collideEngine(mario2)
+  collideBomb(bomb)
 
-	if player.score >= 100 then dofile("transition2.lua") end
-	if player.health <= 0 then dofile("over.lua") end
+  if player.score >= 100 then dofile("transition2.lua") end
+  if player.health <= 0 then dofile("over.lua") end
 
-	screen:blit(0,0,back)
-	screen:blit(player.x,player.y,player.img)
-	screen:fillRect(10,10,player.health,10,Color.new(255,0,0))
-	screen:blit(mario.x,mario.y, mario.img)
-	screen:blit(mario2.x,mario2.y, mario.img)
-	screen:blit(bomb.x,bomb.y, bomb.img)
-	screen:print(380,10,"Score :" ..player.score,Color.new(25,200,0))
-	
-	screen.waitVblankStart()
-	screen.flip()
+  screen:blit(0,0,back)
+  screen:blit(player.x,player.y,player.img)
+  screen:fillRect(10,10,player.health,10,Color.new(255,0,0))
+  screen:blit(mario.x,mario.y, mario.img)
+  screen:blit(mario2.x,mario2.y, mario.img)
+  screen:blit(bomb.x,bomb.y, bomb.img)
+  screen:print(380,10,"Score :" ..player.score,Color.new(25,200,0))
+  
+  screen.waitVblankStart()
+  screen.flip()
 end
