fdbc8ae498677aff86b41aa9f20ea9fee6a807cb
Yonaba <roland.yonaba@gmail.com>
2014-08-08 12:27:18 +0000

    Tabs to 2spaces

--- src/transition.lua
+++ src/transition.lua
@@ -1,12 +1,12 @@
 while true do
-	screen:clear()
-	pad = Controls.read()
+  screen:clear()
+  pad = Controls.read()
 
-	if pad:start() then dofile("lvl2.lua") end
+  if pad:start() then dofile("lvl2.lua") end
 
-	screen:print(220,100,"Level 2", Color.new(255,255,255))
-	screen:print(202,110,"Press Start", Color.new(255,255,255))
+  screen:print(220,100,"Level 2", Color.new(255,255,255))
+  screen:print(202,110,"Press Start", Color.new(255,255,255))
 
-	screen.waitVblankStart()
-	screen.flip()
+  screen.waitVblankStart()
+  screen.flip()
 end
--- src/transition2.lua
+++ src/transition2.lua
@@ -1,12 +1,12 @@
 while true do
-	screen:clear()
-	pad = Controls.read()
+  screen:clear()
+  pad = Controls.read()
 
-	if pad:start() then dofile("lvl3.lua")() end
-	
-	screen:print(220,100,"Level 3", Color.new(255,255,255))
-	screen:print(202,110,"Press Start", Color.new(255,255,255))
+  if pad:start() then dofile("lvl3.lua")() end
+  
+  screen:print(220,100,"Level 3", Color.new(255,255,255))
+  screen:print(202,110,"Press Start", Color.new(255,255,255))
 
-	screen.waitVblankStart()
-	screen.flip()
+  screen.waitVblankStart()
+  screen.flip()
 end
