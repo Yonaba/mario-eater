5839cecefef9352671d35b31898f604eb3380d7f
Yonaba <roland.yonaba@gmail.com>
2014-08-10 12:28:58 +0000

    Tabs to 2spaces

--- src/ended.lua
+++ src/ended.lua
@@ -7,22 +7,22 @@ Mp3me.load("sound.mp3")
 Mp3me.play()
 
 while true do
-	screen:clear()
-	pad = Controls.read()
+  screen:clear()
+  pad = Controls.read()
 
-	if pad:cross() then dofile("script.lua") end
+  if pad:cross() then dofile("script.lua") end
 
-	screen:blit(0,0,back)
-	screen:blit(220,y,player)
+  screen:blit(0,0,back)
+  screen:blit(220,y,player)
 
-	y = y-0.5
+  y = y-0.5
 
-	if y <= 110 then 
-		y = 110 
-		screen:print(215,85,"You win", Color.new(255,0,0))
-		screen:print(202,95,"Press Cross", Color.new(255,0,0))
-	end
-	
-	screen.waitVblankStart()
-	screen.flip()
+  if y <= 110 then 
+    y = 110 
+    screen:print(215,85,"You win", Color.new(255,0,0))
+    screen:print(202,95,"Press Cross", Color.new(255,0,0))
+  end
+  
+  screen.waitVblankStart()
+  screen.flip()
 end
