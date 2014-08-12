1782b326ae70a99d43bcd58d725638b0969fb6c8
Yonaba <roland.yonaba@gmail.com>
2014-08-12 12:29:33 +0000

    Tabs to 2spaces

--- src/script.lua
+++ src/script.lua
@@ -1,13 +1,13 @@
 while true do
-	screen:clear()
-	pad = Controls.read()
+  screen:clear()
+  pad = Controls.read()
 
-	if pad:start() then dofile("lvl1.lua") end
+  if pad:start() then dofile("lvl1.lua") end
 
-	screen:print(150,100,"Eat all Falling Mario To win", Color.new(255,255,255))
-	screen:print(202,110,"Enjoy your meal!!", Color.new(255,255,255))
-	screen:print(380,252,"Press Start", Color.new(255,255,255))
+  screen:print(150,100,"Eat all Falling Mario To win", Color.new(255,255,255))
+  screen:print(202,110,"Enjoy your meal!!", Color.new(255,255,255))
+  screen:print(380,252,"Press Start", Color.new(255,255,255))
 
-	screen.waitVblankStart()
-	screen.flip()
+  screen.waitVblankStart()
+  screen.flip()
 end
